2d2be425e04e4dec0adeea321e2d6c154837b001
quocthien355 <101032064+quocthien355@users.noreply.github.com>
2023-08-23 23:53:29 +0700

    update file config database

--- helpers/config.js
+++ helpers/config.js
@@ -5,6 +5,15 @@ const con = mysql.createConnection({
      password: "DATN2023-",
      database: "vehiclec_bughunter"
 });
+
+var del = connection._protocol._delegateError;
+
+con._protocol._delegateError = function(err, sequence){
+     if (err.fatal) {
+       console.trace('fatal error: ' + err.message);
+     }
+     return del.call(this, err, sequence);
+   };
 con.connect(function(err) {
 if (err) console.error(err);
 console.log("Đã kết nối tới database!!!")
